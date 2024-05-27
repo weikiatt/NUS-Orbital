d86dcca4df891dbd7f308c925057f601dbcd2124
weikiatt <e1122692@u.nus.edu>
2024-05-27 23:12:25 +0800

    test 2

--- NUS Orbital/Controllers/StudentController.cs
+++ NUS Orbital/Controllers/StudentController.cs
@@ -36,7 +36,7 @@ namespace NUS_Orbital.Controllers
             TempData["Login"] = "Login to view account info";
             return RedirectToAction("Login", "Home");
         }
-
+        //
         [HttpPost]
         public async Task<ActionResult> Account(Student student)
         {
